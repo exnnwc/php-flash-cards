be7f2210674f3042bb5fee53e233734b7c4cc5eb
exnnwc <2buzezsn@gmail.com>
2016-02-05 07:02:50 -0800

    Finished the script for functions and cleaned up files.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# PHP Flash Cards \n This is basically a project that makes a comma-separated list using get_defined_function() and then scraping the php.net manual website to get the relevant information about it. \n After it's created the comma-separated list, it's ready to be made into Anki flash cards.
+# PHP Flash Cards 
+ This is basically a project that makes a comma-separated list using get_defined_function() and then scraping the php.net manual website to get the relevant information about it. \n After it's created the comma-separated list, it's ready to be made into Anki flash cards.
+
+I'd like to make the format better inside the cards so if you have any suggestions, please let me know. I spent a lot of time trying to make it better but had to settle for the current mediocrity.
+
+I've already added another script for generating flash cards for the HTTP Status Codes and I'm going to create scripts for other parts of PHP as well. (constants, classes, etc.)
+
+
+
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?PHP
-
+$num_of_functions=0;
 include ("phpQuery-onefile.php");
 define("DEFINITION", "span.dc-title");
 //define ("DEFINITION", "div.refnamediv");
@@ -7,82 +7,61 @@ define("DESCRIPTION", "div.description");
 define("PARAMETERS", "div.parameters");
 define("RETURN", "div.returnvalues");
 $invalid_keys = ["HDOM", "DEFA", "MAX_"];
-$string = "";
-$arr = get_defined_functions()["internal"];
-sort($arr);
+$function_list = get_defined_functions()["internal"];
+sort($function_list);
 if (!isset($_GET['id'])) {
-    $id = rand(0, sizeof($arr));
+    $id = rand(0, sizeof($function_list));
 } else {
     $id = $_GET['id'];
 }
-//$random_id=rand(1,sizeof($arr));
 
-/* echo "<div>
-  <a href='".$_SERVER['PHP_SELF']."?id=".($id-1)."'>Prev</a>
-  <a href='". $_SERVER['PHP_SELF'] ."?id=$random_id'>Random </a>
-  <a href='".$_SERVER['PHP_SELF']."?id=".($id+1)."'>Next</a>
-  </div>";
-
-  echo "<div><a target='_blank' href='$function_url'>$arr[$id]</a></div>";
- */
-
-$file = fopen("shit.txt", "w");
+$file = fopen("php_functions.csv", "w");
 
 $fields = [];
-error_reporting(E_ALL &  ~E_WARNING);
+error_reporting(E_ALL &  ~E_WARNING);//sizeof($function_list)
 for ($i = 0; $i < 3; $i++) {
-    $id = rand(0, sizeof($arr));
-    $function_url = "http://php.net/manual/en/function." . str_replace("_", "-", $arr[$id]) . ".php";
+    $string = "";
+    $function_url = "http://php.net/manual/en/function." . str_replace("_", "-", $function_list[$i]) . ".php";
     $html = phpQuery::newDocumentFile($function_url);
 	if(strpos(error_get_last()["message"], "404 Not Found")==false){	
-	
-	echo "Fetching $function_url... <br /><br />";
-        $name = $arr[$id];
+        $num_of_functions++;
+   	echo "Fetching $function_url... \n";
+        $name = $function_list[$i];
         foreach (get_defined_constants(true)["user"] as $key => $value) {
             if (!in_array(substr($key, 0, 4), $invalid_keys)) {
-                foreach (pq(constant($key)) as $element)
-                    $string = $string .  process_string($element->textContent, $name) ;       
+                foreach (pq(constant($key)) as $element){
+                   $string = $string .  process_string($element->textContent, $name) ;       
+                }
             } 
         } 
         $info = $string;
-	echo "<span style='font-weight:bold'>$arr[$id]</span> <BR> $info";
-    fwrite($file, "\"$name\", \"$info\" \n");
+    	echo "<span style='font-weight:bold'>$function_list[$id]</span> <BR> $info";
+        fwrite($file, "\"$name\", \"$info\" \n");
     } else {
-	echo "<BR>$arr[$id] - $function_url<BR>";
-        $i--;
+    	echo "$function_list[$i] - $function_url does not exist.\n";
     }
+    sleep(1);
 }
-
-        
-        
-
 fclose($file);
-
+echo "$num_of_functions out of ".sizeof($function_list) ." functions have been processed.";
 function process_string($string, $name){
-$EOL = ["</div>", "</h3>", "</dt>", "</dd>", "</p>"];
-$bold_words=["Description", "Parameters", "Return Values"];
-$delete = ["/[<>]/","/\W$name\W/"];
-$newline_trim=["SHITSHIT"];
-$string=  
-  preg_replace("/SHIT\s+SHIT/", " ",
-    str_replace("SHITSHIT", "SHIT",
-      str_replace("\n", "SHIT",
-        preg_replace($delete, "", 
-          strip_tags(
-	    preg_replace($EOL, "\n", 
-	      $string))))));
-
-for ($i=0;$i<10;$i++){
-}
-foreach ($bold_words as $bold_word){
-	$string=str_replace("$bold_word", "<span style='font-weight:bold'>$bold_word</span>", $string);
-}
-
-//$string=htmlspecialchars($string);
-return $string;
-
-
+    $EOL = ["</div>", "</h3>", "</dt>", "</dd>", "</p>"];
+    $bold_words=["Description", "Parameters", "Return Values"];
+    $delete = ["/[<>]/","/\W$name\W/"];
+/*    $string=  
+      str_replace ("Procedural style ", "<br />Procedural style ",
+        preg_replace("/XPLACEHOLDERX\s+XPLACEHOLDERX/", " ",
+          str_replace("XPLACEHOLDERX", "<BR>", 
+            str_replace("XPLACEHOLDERXXPLACEHOLDERX", "XPLACEHOLDERX",
+                preg_replace($delete, "", 
+                  htmlspecialchars(
+                    strip_tags(
+          	          preg_replace($EOL, "XPLACEHOLDERX", 
+          	            $string))))))));*/
+    $string = str_replace("$name", "NAME", $string);
+    foreach ($bold_words as $bold_word){
+    	$string=str_replace("$bold_word", "</pre><div style='font-weight:bold;margin-top:16px;'>$bold_word</div><pre>", $string);
+    }
+    return $string;
 }
 
-function show_html(){
-}
